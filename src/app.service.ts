e11d83c59fcd6c507e26f98a7a34c3e0874ec9ba
ikaranloona <karan.loona@deel.com>
2024-02-12 01:31:14 +0530

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World by deel';
+    return 'Hello World by deel with auth';
   }
 }
